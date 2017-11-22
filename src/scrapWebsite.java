3d35a88e5d04a752a3139820bd1811e9646c55b8
VeritasCurat <johannesgrobelski@live.de>
2017-11-22 12:22:01 +0100

    -
    -

--- src/scrapWebsite.java
+++ src/scrapWebsite.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -16,6 +18,9 @@ public class scrapWebsite {
 		Book testInfoBuch = new Book();
 		Book testAehnlicheBuecher = new Book("testtitel", "testautor", "testpub", "testblurb", 0.1);
 		BookList testBuchliste = new BookList();
+		
+		fetchCharacterList();
+		
 
 		//infosBuecher(testInfoBuch,"Metro 2033");
 		//ThemaZuBuecherliste("Horror",testBuchliste);
@@ -25,6 +30,66 @@ public class scrapWebsite {
 
 
 		//sammelBuchInformation("Metro 2033");
+	}
+	//
+	
+	private static boolean isInteger(String s, int radix) {
+	    if(s.isEmpty()) return false;
+	    for(int i = 0; i < s.length(); i++) {
+	        if(i == 0 && s.charAt(i) == '-') {
+	            if(s.length() == 1) return false;
+	            else continue;
+	        }
+	        if(Character.digit(s.charAt(i),radix) < 0) return false;
+	    }
+	    return true;
+	}
+	
+	public static void fetchCharacterList() throws IOException {
+		//anzahl pages 
+			Elements elements = Jsoup.connect("https://www.goodreads.com/characters").get().select("a");
+			int max=0;
+			for(Element el: elements) {
+			 if(isInteger(el.text(),10) && max < Integer.parseInt(el.text()))max = Integer.parseInt(el.text());
+			}
+				//System.out.println(max);
+		String 	x=""; 
+			
+		//durch pages gehen
+		for(int page=1; page<=2; page++) {
+			 elements = Jsoup.connect("https://www.goodreads.com/characters?page="+page).get().select("div.elementList"); //. selektiert klassen (mit angegebenen namen) des tags vor dem punkt
+			for(Element el: elements) {
+					byte ptext[] = el.text().getBytes();
+					String name = new String(ptext, "UTF-8");
+					//TODO: namen mit nicht lateinischen zeichen werden rausgeschmissen (wegen ersetzung durch ?) -> fix
+						boolean ok=false;
+						char[] chars = name.toCharArray();
+					    for (char c : chars) {
+					        if(Character.isLetter(c)) {
+					        	ok = true; break;
+					        }
+					    }	
+					    if(!ok)continue;
+					//System.out.println(value);
+					
+					String href = "https://www.goodreads.com"+el.select("a").attr("href");                                                           //attr Attribut selektiert <tag Attribut=....> 
+					//System.out.println(href);
+					x += name + " URL: " + href +"\n";
+			}
+			System.out.println(page);
+		}
+		
+		try {
+		    BufferedWriter out = new BufferedWriter(new FileWriter("characters.txt"));
+		    out.write(x);  //Replace with the string 
+		                                             //you are trying to write
+		    out.close();
+		}
+		catch (IOException e)
+		{
+		    System.out.println("Exception ");
+
+		}
 	}
 
 
