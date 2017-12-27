e46e165753bf965becb497498dc34e7492dc2bfe
VeritasCurat <johannesgrobelski@live.de>
2017-12-27 18:36:53 +0100

    minor fixes

--- src/Book.java
+++ src/Book.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.text.Document;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Element;
@@ -44,7 +46,7 @@ public class Book implements Serializable {
   	private static void test() throws UnsupportedEncodingException, IOException {
   	//Name(Buch) -> Autor, aehnlicheBücher, Charaktere, Genre
   		ArrayList<String> b= buchZuAehnlicheBuecher("Holy Bible: King James Version", "");
-//  		for(String a: b)System.out.println(a);
+  			for(String a: b)System.out.println(a);
   		//printBook(buchToinfosBuecher("The Name of the Wind (The Kingkiller Chronicle, #1)","",""));
   			//a=  aehnlicheBuecher(testAehnlicheBuecher,"Metro 2033","");
   			//infosBuecher(testInfoBuch,"Metro 2033");
@@ -79,7 +81,7 @@ public class Book implements Serializable {
         this.rating = k.rating;
         this.awards = k.awards;
         this.shelves= k.shelves;
-        this.similar_Books=k.similar_Books;
+        //this.similar_Books=k.similar_Books;
         this.Characters=k.Characters;
         this.Author=k.Author;
 	}
@@ -95,9 +97,9 @@ public class Book implements Serializable {
 		for (String x: Buchmeta.Author)System.out.println(x);
 		System.out.println("________________________________________________");
 		System.out.println("Similar Books: \n");
-		for (String x: Buchmeta.similar_Books)	System.out.println(x);
-		System.out.println("________________________________________________");
-		System.out.println("Characters in the Book: \n");
+			for (String x: Buchmeta.similar_Books)	System.out.println(x);
+			System.out.println("________________________________________________");
+			System.out.println("Characters in the Book: \n");
 		for (String x: Buchmeta.Characters) System.out.println(x);
 		System.out.println("________________________________________________");
 		System.out.println("Genre of the Book: \n");
@@ -109,6 +111,34 @@ public class Book implements Serializable {
 		System.out.println("Blurb of the Book: \n");
 		System.out.println(Buchmeta.blurb);
 	}
+    
+    public static ArrayList<Book> Schnitt(ArrayList<Book> a, ArrayList<Book> b){
+    	ArrayList<Book> results = new ArrayList<>();
+    	for(Book q: a) {
+    		if(b.contains(q))results.add(q);
+    	}
+    	return results;
+    }
+    
+    public static ArrayList<Book> Vereinigung(ArrayList<Book> a, ArrayList<Book> b){
+    	ArrayList<Book> results = new ArrayList<>();
+    	for(Book q: a) {
+    		results.add(q);
+    	}
+    	for(Book p: b) {
+    		if(!results.contains(p))results.add(p);
+    	}
+    	return results;
+    }
+    
+    public static ArrayList<String> removeDoubles(ArrayList<String> a){
+    	ArrayList<String> b = new ArrayList<>();
+    	for(String s: a) {
+    		if(b.contains(s))continue;
+    		else b.add(s);
+    	}
+    	return b;
+    }
 
     public static Book buchToinfosBuecher(String title, String author, String url) throws UnsupportedEncodingException, IOException {
 		Book book = new Book();
@@ -322,35 +352,35 @@ public class Book implements Serializable {
 		//finde link zu buch ~ rufe dafÃ¼r 1 ergebnis 
 			String search_1 = "https://www.goodreads.com/search?page=1&query=" + title + " " + author + "&tab=books&utf8=%E2%9C%93";
 			org.jsoup.nodes.Document doc = Jsoup.connect(search_1 + URLEncoder.encode(search_1, "UTF-8")).get();
-		
 			if(doc.select("h3.searchSubNavContainer").toString().toLowerCase().contains("no results")) return null; 
 		
-			org.jsoup.select.Elements first = doc.getElementsByTag("td").select("a");
-			System.out.println(first.html());
-				//String link = "https://www.goodreads.com." + first.html().substring(first.html().indexOf("href") + 6, first.html().indexOf(">", first.html().indexOf("href")) - 1);
-			String link = first.select("link").text();System.out.println(link);
-			doc = Jsoup.connect(link).userAgent("bot101").get();
-			//TODO: funzt u.u. nicht immer
-			String linktext_schlecht = doc.html().substring(doc.html().indexOf("/trivia/work/") + 13, doc.html().indexOf("\"", doc.html().indexOf("/trivia/work/"))); //System.out.println("link: "+linktext_ann1);
-
-		//similar aufrufen
-			String similar = "https://www.goodreads.com/book/similar/" + linktext_schlecht;
-			//System.out.println(similar);
-			doc = Jsoup.connect(similar).get();
-
-		//liste der Ã¤hnlichen bÃ¼cher sammeln
-			org.jsoup.select.Elements results_doc = doc.getElementsByTag("tr");
-			for (Element result_doc : results_doc) {
-				//TODO: link-teil furchtbar, auswahl Ã¼ber html string. gibt es mÃ¶glichtkeit das <href>-tag des <title> tag auszuwÃ¤hlen??
-				results.add(result_doc.text());//.substring(0, result_doc.text().lastIndexOf("–")) + "\n link:" + result_doc.html().substring(result_doc.html().indexOf("href") + 6, result_doc.html().indexOf(">", result_doc.html().indexOf("href")) - 1));
+			String link_book = "https://www.goodreads.com"+doc.select("a.booktitle").first().attr("href");
+				System.out.println(link_book);
+			org.jsoup.nodes.Document doc_book = Jsoup.connect(link_book).userAgent("bot101").get();
+			
+			//TODO: immer noch nicht perfekt, aber besser als vorher, da keine substringsuche
+			String similar_link = "https://www.goodreads.com/book/similar/";
+			for(Element e:doc_book.select("div").select("h2.brownBackground").select("a")) {
+				if(e.attr("href").contains("work")) { //contains("work") ist kritisch
+					similar_link += e.attr("href").replaceAll("[^0-9]+", "");
+					break;
+				}
 			}
+			System.out.println(similar_link);
+		//similar aufrufen
+			doc = Jsoup.connect(similar_link).get();
 
-		return results;
+		//liste der Ã¤hnlichen buecher sammeln
+			org.jsoup.select.Elements results_doc = doc.select("div").select("a");
+			for (Element result_doc : results_doc) {
+				String link = result_doc.attr("href").replaceAll(" ", "");
+				if(link.contains("/book/show/") && !results.contains(link))results.add("https://www.goodreads.com"+link);
+			}
+		return removeDoubles(results);
 	}
     
 
     //Setter, adder etc.
-						
 						public void setPublisher(String publisher) {
 							this.publisher = publisher;
 						}
@@ -385,10 +415,11 @@ public class Book implements Serializable {
 					    public void setSimilar_Books(List<String> similarBuch) {
 					        similar_Books.addAll(similarBuch) ;
 					    }
-					
+				
 					    public void addAehnlichBuch(String bookname) {
 					        if (!similar_Books.contains(bookname)) similar_Books.add(bookname);
 					    }
+					    
 					    public void removeAehnlichBuch(String bookname){
 					        if(shelves.contains(bookname)) shelves.remove(bookname);
 					    }
